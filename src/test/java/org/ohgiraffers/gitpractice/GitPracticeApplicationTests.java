7edfbea5a99ad4d6677c811e9c0fab1b92abfd67
thdrkfka <ssongjiaran@gmail.com>
2024-02-16 09:51:31 +0900

    First commit

---
+++ src/test/java/org/ohgiraffers/gitpractice/GitPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ohgiraffers.gitpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
